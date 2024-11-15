35d6aff4fe5371526ca0c9e717eef18fdaf86d28
leandro <leandro.korb69@gmail.com>
2024-11-15 01:21:41 -0300

    se agrego nodemailer y router para enviar mail al usuario

--- src/config.js
+++ src/config.js
@@ -1,11 +1,22 @@
-const PORT = process.env.PORTDEV || 8081
-const STRC = process.env.STRCDEV
-const NAMEBASE = process.env.NAMEBASEDEV
-const PERSISTENCE = process.env.PERSISTENCEDEV || ""
+import nodeMailer from "nodemailer";
+import { EMAIL_USER, EMAIL_PASS } from ".env";
+
+const PORT = process.env.PORTDEV || 8081;
+const STRC = process.env.STRCDEV;
+const NAMEBASE = process.env.NAMEBASEDEV;
+const PERSISTENCE = process.env.PERSISTENCEDEV || "";
+const TRANSPORTER = nodeMailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: EMAIL_USER,
+    pass: EMAIL_PASS,
+  },
+});
 
 export default {
-    PORT,
-    STRC,
-    NAMEBASE,
-    PERSISTENCE
-}
+  PORT,
+  STRC,
+  NAMEBASE,
+  PERSISTENCE,
+  TRANSPORTER,
+};
--- src/routes/UsersRoute.js
+++ src/routes/UsersRoute.js
@@ -1,22 +1,23 @@
-import UserController from "../controllers/UsersController.js"
-import express from "express"
+import UserController from "../controllers/UsersController.js";
+import express from "express";
 
-class UserRoutes{
-    constructor(){
-        this.router = express.Router()
-        this.controller = new UserController()
-    }
+class UserRoutes {
+  constructor() {
+    this.router = express.Router();
+    this.controller = new UserController();
+  }
 
-    start(){
-        this.router.get("/user", this.controller.getUser)
-        this.router.get("/user/:id", this.controller.getUserById)
-        this.router.post("/user", this.controller.postUser)
-        this.router.patch("/user/update/:id", this.controller.patchUser)
-        this.router.put("/user/update/all/:id", this.controller.putUser)
-        this.router.delete("/user/:id", this.controller.deleteUser)
+  start() {
+    this.router.get("/user", this.controller.getUser);
+    this.router.get("/user/:id", this.controller.getUserById);
+    this.router.post("/user", this.controller.postUser);
+    this.router.patch("/user/update/:id", this.controller.patchUser);
+    this.router.put("/user/update/all/:id", this.controller.putUser);
+    this.router.delete("/user/:id", this.controller.deleteUser);
+    this.router.post("/user/send-mail", this.controller.postUserSendMail);
 
-        return this.router
-    }
+    return this.router;
+  }
 }
 
-export default UserRoutes
+export default UserRoutes;
