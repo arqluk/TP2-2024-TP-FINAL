d195c2121f6ef3780fb81ae618e307bb39af8ffd
vicabaum <vickybaumgertner@gmail.com>
2024-11-17 15:36:29 -0300

    Probé JSDoc y agregué la documentación de todo el proyecto

--- src/routes/ProductsRoute.js
+++ src/routes/ProductsRoute.js
@@ -1,23 +1,42 @@
 import ProdController from "../controllers/ProductsController.js"
 import express from "express"
 
+/**
+ * Clase que maneja las rutas de productos.
+ * 
+ * Esta clase define las rutas HTTP para crear, obtener, actualizar y eliminar productos.
+ */
 class ProdRoutes {
     constructor() {
-        this.router = express.Router()
-        this.controller = new ProdController()
+        /**
+         * El enrutador de Express utilizado para definir las rutas.
+         * @type {Object}
+         */
+        this.router = express.Router();
+
+        /**
+         * El controlador de productos que gestiona la lógica de las operaciones.
+         * @type {ProdController}
+         */
+        this.controller = new ProdController();
     }
 
+    /**
+     * Inicia las rutas de productos.
+     * 
+     * @returns {Object} El router con las rutas configuradas.
+     */
     start() {
-        this.router.get("/prod", this.controller.getProd)
-        this.router.get("/prod/:id", this.controller.getProdById)
-        this.router.post("/prod", this.controller.postProd)
-        this.router.patch("/prod/update/:id", this.controller.patchProd)
-        this.router.put("/prod/update/all/:id", this.controller.putProd)
-        this.router.delete("/prod/:id", this.controller.deleteProd)
-        this.router.get('/prod/priceInARS/:id', this.controller.getProdPriceInARS)
+        this.router.get("/prod", this.controller.getProd); // Obtener todos los productos
+        this.router.get("/prod/:id", this.controller.getProdById); // Obtener un producto por ID
+        this.router.post("/prod", this.controller.postProd); // Crear un producto
+        this.router.patch("/prod/update/:id", this.controller.patchProd); // Actualizar parcialmente un producto
+        this.router.put("/prod/update/all/:id", this.controller.putProd); // Actualizar completamente un producto
+        this.router.delete("/prod/:id", this.controller.deleteProd); // Eliminar un producto
+        this.router.get("/prod/priceInARS/:id", this.controller.getProdPriceInARS); // Obtener el precio en ARS de un producto
 
-        return this.router
+        return this.router;
     }
 }
 
-export default ProdRoutes
+export default ProdRoutes;
