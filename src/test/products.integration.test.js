6331e31877c6c76512c8f1d32c425589b6043210
arqluk <alejandro.lukacher@gmail.com>
2024-11-17 12:44:24 -0300

    se agregaron los tests. Error de heap memory

---
+++ src/test/products.integration.test.js
@@ -0,0 +1,19 @@
+import generator from "../utils/generator.js"
+import { expect } from "chai"
+import supertest from "supertest"
+
+const urlBase = supertest("http://localhost:8080")
+
+describe('Test de productos: ', () => {
+    const data = generator.randomProd()
+
+    it('GET Products', async () => {
+        const response = await urlBase.get("/prod")
+        expect(response.status).to.equal(200)
+    })
+
+    it('POST Products', async () => {
+        const response = await urlBase.post("/prod").set('role', 'admin').send(data)
+        expect(response.status).to.equal(200)
+    })
+}) 
